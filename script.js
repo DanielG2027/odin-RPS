9a6551101403f12c268ebfbe5e99bcd14a51fcb7
DanielG2027 <d.garcia8021@gmail.com>
2026-02-16 23:38:29 -0800

    Added UI to game

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(function(btn) {
+    btn.addEventListener('click', event => {
+        const humanSelection = event.target.id;
+        playGame(humanSelection, getComputerChoice());
+    })
+});
+const results = document.querySelector('#results');
+const resultMessage = document.createElement('h3');
+const score = document.createElement('h2');
+results.appendChild(score);
+results.appendChild(resultMessage); 
+
+// Game logic for Rock, Paper, Scissors
 function getComputerChoice(){
     const intRand = Math.random();
     let rpsSelect = null;
@@ -13,53 +28,49 @@ function getComputerChoice(){
     return rpsSelect;
 }
 
-function getHumanChoice(){
-    const humanInput = prompt("Rock, Paper, or Scissors?");
-    return humanInput;
-}
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    
-    function playRound(humanChoice, computerChoice){
-    humanChoice.toLowerCase();
-    if (humanChoice == "rock" && computerChoice == "scissors" || 
-    humanChoice == "paper" && computerChoice == "rock" || 
-    humanChoice == "scissors" && computerChoice == "paper"){
-        humanScore ++;
-        console.log("You won! " + humanChoice.toUpperCase() + " beats " + computerChoice.toUpperCase());
-        return;
-    }
-    else if (humanChoice == "paper" && computerChoice == "scissors"
-        || humanChoice == "rock" && computerChoice == "paper"
-        || humanChoice == "scissors" && computerChoice == "rock"){
-        computerScore ++;
-        console.log("You lost! " + computerChoice.toUpperCase() + " beats " + humanChoice.toUpperCase());
-        return;
-    }
-    else {
-        console.log("You tied! " + humanChoice.toUpperCase() + " doesn't beat " + computerChoice.toUpperCase());
-        return;
-    }
-}
-    for(var i = 1; i <= 5; i++){
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    if(humanScore > computerScore){
-        console.log("You won! \nYou beat the computer " + humanScore + " to " + computerScore);
-    }
-    else if(computerScore > humanScore){
-        console.log("You lost! The score was " + computerScore + " to " + humanScore);
-    }
-    else{
-        console.log("It was so close! Try again to beat the computer. \nThe score was " + humanScore + " to " + computerScore);
+function playRound(humanChoice, computerChoice){
+    switch (`${humanChoice}-${computerChoice}`) {
+        case "rock-scissors":
+        case "paper-rock":
+        case "scissors-paper":
+            humanScore++;
+            score.textContent = `${humanScore}\t ${computerScore}`
+            resultMessage.textContent = `You won!\n ${humanChoice.toUpperCase()} beats ${computerChoice.toUpperCase()}`;
+            break;
+        case "paper-scissors":
+        case "rock-paper":
+        case "scissors-rock":
+            computerScore++;
+            score.textContent = `${humanScore}\t ${computerScore}`;
+            resultMessage.textContent = `You lost!\n ${computerChoice.toUpperCase()} beats ${humanChoice.toUpperCase()}`;
+            break;
+        default:
+            resultMessage.textContent = `You tied!\n ${humanChoice.toUpperCase()} doesn't beat ${computerChoice.toUpperCase()}`;
+            score.textContent = `${humanScore}\t ${computerScore}`
+            break;
     }
 }
 
-playGame();
+function playGame(humanSelection, computerSelection){
+    playRound(humanSelection, computerSelection);
+
+    if(humanScore >= 5 || computerScore >= 5){
+        if(humanScore > computerScore){
+            resultMessage.textContent = `\nCongratulations! \nYou beat the computer ${humanScore} to ${computerScore}`;
+        }
+        else if(computerScore > humanScore){
+            resultMessage.textContent = `\nThat's too bad! \nThe score was ${humanScore} to ${computerScore}`;
+        }
+        else{
+            resultMessage.textContent = `\nIt was so close! Try again to beat the computer. \nThe score was ${humanScore} to ${computerScore}`;
+        }
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
 
 
 
